cc7f6bc318562907bd07e33076f0a2f3b17fbeb0
pseudobabble <harryjohnson500@gmail.com>
2021-01-17 15:42:43 +0000

    basic block model

---
+++ chunk_dynasty/core/chunk.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from typing import Callable
+from Cryptodome.Hash import SHA256
+
+
+def get_salter(data: bytes, parent_header: bytes) -> Callable:
+    unsalted = data + parent_header
+    return lambda salt: SHA256.new(data=(unsalted + salt).encode()).hexdigest()
+
+
+class Chunk:
+    def __init__(
+            self, data: bytes, parent_header: bytes, header: bytes, salt: bytes
+    ) -> None:
+        self._data = data
+        self._parent_header = parent_header
+        self._header = header
+        self._salt = salt
+
+    def __str__(self) -> str:
+        return self._parent_header + self._data + self._salt
+
+    @property
+    def data(self) -> bytes:
+        return self._data
+
+    @property
+    def header(self) -> bytes:
+        return _header
+
+    @property
+    def parent_header(self) -> bytes:
+        return self._parent_header
+
+    @property
+    def salt(self) -> bytes:
+        return self._salt
