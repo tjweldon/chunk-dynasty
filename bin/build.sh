7e310bc527729a14741e79b9acefb7945fd85b9e
Tom Weldon <thomas.weldon@legl.com>
2021-03-30 22:17:17 +0100

    Set up basic supervisor envirnment for miner

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
 pip install -r /app/requirements.txt -q 2>&1
+python /app/initdb.py
+/usr/bin/supervisord
 tail -f /dev/null
---
+++ initdb.py
@@ -0,0 +1,4 @@
+from persistence.migrations import init_db
+
+if __name__ == "__main__":
+    init_db.run()
